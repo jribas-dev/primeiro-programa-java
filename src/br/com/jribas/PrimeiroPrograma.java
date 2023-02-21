57839eea452330b7dc06a7be0541ef0e784ca04e
jribas-dev <3b3brazil@gmail.com>
2023-02-20 23:45:28 -0300

    first commit

---
+++ src/br/com/jribas/PrimeiroPrograma.java
@@ -0,0 +1,25 @@
+package br.com.jribas;
+
+import br.com.jribas.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		/* 
+		   int a = 2;
+		   int b = 3;
+		   System.out.println("Hello World! " + (a+b));
+		*/
+	   Gato gato = new Gato("Garfield", "Marrom", 35);
+	   Livros livro = new Livros();
+	   
+	   System.out.println(gato);
+	   System.out.println(livro);
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
