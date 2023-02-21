d4a85c6b5ddb51c9ebf3e24d74acfaa0e65e3196
jribas-dev <3b3brazil@gmail.com>
2023-02-21 01:45:38 -0300

    adicionado get setters na classe Livros

--- src/br/com/curso/PrimeiroPrograma.java
+++ src/br/com/curso/PrimeiroPrograma.java
@@ -1,5 +1,7 @@
 package br.com.curso;
 
+import java.util.Objects;
+
 import br.com.curso.model.Gato;
 
 public class PrimeiroPrograma {
@@ -22,4 +24,51 @@ public class PrimeiroPrograma {
 class Livros {
 	private String nome;
 	private String npag;
+	
+	public Livros() {
+	}
+
+	public Livros(String nome, String npag) {
+		this.nome = nome;
+		this.npag = npag;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNpag() {
+		return npag;
+	}
+
+	public void setNpag(String npag) {
+		this.npag = npag;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nome, npag);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livros other = (Livros) obj;
+		return Objects.equals(nome, other.nome) && Objects.equals(npag, other.npag);
+	}
+
+	@Override
+	public String toString() {
+		return "Livros [nome=" + nome + ", npag=" + npag + "]";
+	}
+	
 }
